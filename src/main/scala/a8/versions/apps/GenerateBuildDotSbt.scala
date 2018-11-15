68eeb7ade8d288d23da51f3411e698a9fcc1e68a
glen@roar <glen@accur8software.com>
2018-11-15 15:56:11 -0500

    for build_dot_sbt added MACHINE GENERATED comments/warnings on the top of generated files

--- src/main/scala/a8/versions/apps/GenerateBuildDotSbt.scala
+++ src/main/scala/a8/versions/apps/GenerateBuildDotSbt.scala
@@ -5,6 +5,7 @@ import a8.common.CommonOps._
 import a8.versions.BuildDotSbtGenerator
 import a8.versions.apps.Main.Config
 import a8.versions.model.CompositeBuild
+import m3.predef._
 
 object GenerateBuildDotSbt extends App {
 
@@ -12,7 +13,7 @@ object GenerateBuildDotSbt extends App {
 
 //  run("ahs_aggregate", homeDir \\ "code/ahs/aggregate")
 
-//  run("manna", homeDir \\ "code/manna")
+  run(homeDir \\ "code/manna")
 //  run("model3", homeDir \\ "code/model3")
 //  run("qubes", homeDir \\ "code/qubes")
 //  run("ahs_scala", homeDir \\ "code/ahs-scala")
@@ -20,7 +21,7 @@ object GenerateBuildDotSbt extends App {
 //  run("honeybadger", homeDir \\ "code/build-tools/honeybadger")
 //  run("s3_backup", homeDir \\ "code/s3-backup")
 
-  run(homeDir \\ "code/aggregates")
+//  run(homeDir \\ "code/aggregates")
 
 
   def run(d: m3.fs.Directory) = {
