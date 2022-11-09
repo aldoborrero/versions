f3a613b5a62182efc9bc78919c3dfc55526aba6e
glen@stella <glen@accur8software.com>
2022-11-09 10:40:23 -0500

    fixes for supervisor config generation corrections

--- src/main/scala/io/accur8/neodeploy/PathAssist.scala
+++ src/main/scala/io/accur8/neodeploy/PathAssist.scala
@@ -1,21 +1,16 @@
 package io.accur8.neodeploy
 
 import a8.shared.FileSystem.{File, Path}
+import a8.shared.app.LoggingF
 import zio.{Task, ZIO}
 
 import java.nio.file.Files
 
-object PathAssist {
-
+object PathAssist extends LoggingF {
 
   def symlink(target: Path, link: File): Task[Unit] =
     ZIO.attemptBlocking {
-      try {
-        Files.createSymbolicLink(link.asNioPath, target.asNioPath)
-      } catch {
-        case e: Exception =>
-          e.toString
-      }
+      Files.createSymbolicLink(link.asNioPath, target.asNioPath)
     }
 
 }
--- src/main/scala/io/accur8/neodeploy/SupervisorSync.scala
+++ src/main/scala/io/accur8/neodeploy/SupervisorSync.scala
@@ -4,8 +4,9 @@ package io.accur8.neodeploy
 import a8.shared.FileSystem.{Directory, File}
 import zio.{Task, ZIO}
 import a8.shared.SharedImports._
-import a8.shared.ZString
-import io.accur8.neodeploy.model.{SupervisorDirectory}
+import a8.shared.{FileSystem, ZString}
+import a8.shared.ZString.ZStringer
+import io.accur8.neodeploy.model.SupervisorDirectory
 import io.accur8.neodeploy.resolvedmodel.ResolvedApp
 
 case class SupervisorSync(supervisorDir: SupervisorDirectory) extends ConfigFileSync[ResolvedApp] {
@@ -17,31 +18,47 @@ case class SupervisorSync(supervisorDir: SupervisorDirectory) extends ConfigFile
 
   override def configFileContents(input: ResolvedApp): Task[Option[String]] =
     zsucceed(
-      supervisorConfigContents(input.descriptor).some
+      supervisorConfigContents(input).some
     )
 
-  def supervisorConfigContents(applicationDescriptor: model.ApplicationDescriptor) = {
-    import applicationDescriptor._
-    val resolvedAutoStart = autoStart.getOrElse(true)
+  def supervisorConfigContents(app: ResolvedApp) = {
+
+    def pathZStringer[A <: a8.shared.FileSystem.Path]: ZStringer[A] =
+      new ZStringer[A] {
+        override def toZString(a: A): ZString =
+          a.asNioPath.toAbsolutePath.toString
+      }
+
+    implicit val dirZStringer = pathZStringer[Directory]
+    implicit val fileZStringer = pathZStringer[File]
+
+    import app.descriptor._
+    val resolvedAutoStart = app.descriptor.autoStart.getOrElse(true)
+    val appsRoot = app.server.appsRootDirectory.unresolvedDirectory
+    val bin = appsRoot.subdir("bin").file(app.descriptor.name.value)
+    val logsDir = appsRoot.subdir("logs")
+    val appDir = appsRoot.subdir(app.descriptor.name.value)
+    val tempDir = appDir.subdir("tmp")
     val commandArgs: Seq[String] =
       Seq[ZString](
-      z"/opt/bin/${applicationDescriptor.name}",
+      z"${bin}",
         "-cp",
         z"'lib/*'",
-        z"-Dlog.dir=/opt/logs -Djava.io.tmpdir=/opt/${applicationDescriptor.name}/tmp/",
+        z"-Dlog.dir=${logsDir}",
+        z"-Djava.io.tmpdir=${tempDir}",
       ).map(_.toString()) ++
         jvmArgs ++
         Seq[ZString](
-          z"-Dapp.name=${applicationDescriptor.name}",
+          z"-Dapp.name=${app.descriptor.name}",
           z"${mainClass}",
         ).map(_.toString()) ++
         appArgs
     z"""
-[program:${applicationDescriptor.name}]
+[program:${app.descriptor.name}]
 
 command = ${commandArgs.mkString(" ")}
 
-directory = /opt/${applicationDescriptor.name}
+directory = ${appDir}
 
 autostart       = ${resolvedAutoStart}
 autorestart     = ${resolvedAutoStart}
